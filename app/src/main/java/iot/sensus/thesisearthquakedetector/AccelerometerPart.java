b4467653dbd1324b76f8a57bede3c0b1ec624c9b
Lance-Abais <ljda07@yahoo.com>
2018-01-24 09:21:46 +0800

    Added Accelerometer Service

--- app/src/main/java/iot/sensus/thesisearthquakedetector/AccelerometerPart.java
+++ app/src/main/java/iot/sensus/thesisearthquakedetector/AccelerometerPart.java
@@ -10,8 +10,18 @@ import android.hardware.SensorManager;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
 import java.lang.reflect.Type;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Levy on 1/22/2018.
@@ -20,11 +30,12 @@ import java.util.List;
 public class AccelerometerPart extends Service implements SensorEventListener {
     private SensorManager mSensorManager = null;
     private Sensor mSensor = null;
-    private double mLastX = 0, mLasty = 0;
+    private double mLastX = 0, mLastY = 0;
     private List<Double> mData5s;
     private List<Long> mTime5s;
     private long mStartTime = 0;
     private long mLastUpdate = 0;
+    private String url;
 
     @Nullable
     @Override
@@ -57,19 +68,58 @@ public class AccelerometerPart extends Service implements SensorEventListener {
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         Sensor mAccelerometer = sensorEvent.sensor;
-        if(mAccelerometer == Sensor.TYPE_LINEAR_ACCELERATION){
+        if(mAccelerometer.getType() == Sensor.TYPE_LINEAR_ACCELERATION){
             float x = sensorEvent.values[0];
             float y = sensorEvent.values[1];
 
             long currTime = System.currentTimeMillis();
 
-            if(currTime-mLastUpdate>5000){
-                //TODO::put volley
+            if(mData5s.size()==500){
+                Map<Long,Double> params = new HashMap<>();
+                for(int i=0; i<mData5s.size(); i++) params.put(mTime5s.get(i),mData5s.get(i));
+
+                JSONObject jsonObject = new JSONObject(params);
+
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                        Request.Method.GET, url, jsonObject, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                }
+                );
+
+                Volley.newRequestQueue(this).add(jsonObjectRequest);
+                mData5s.clear();
+                mTime5s.clear();
+
 
             }
 
             if(currTime-mLastUpdate>10){
-                //TODO::put accelerometer process to displacement
+
+                long diffTime = currTime-mLastUpdate;
+                mLastUpdate = currTime;
+
+                double disX = (double) (mLastX*diffTime*diffTime-0.5*x*diffTime);
+                double disY = (double) (mLastY*diffTime*diffTime-0.5*y*diffTime);
+
+                double disXY = (double) (Math.sqrt(Math.pow(disX,2)+Math.pow(disY,2)));
+
+
+                mData5s.add(disXY);
+
+                mTime5s.add(currTime-mStartTime);
+
+                mLastX=x;
+                mLastY=y;
+
+
 
             }
         }
